23ad1b7a714e9d18c585bfdd74bb3fc58d44089b
Neil Marion dela Cruz <nmfdelacruz@gmail.com>
2013-12-07 06:54:03 +0800

    Retrieving photos from library

--- www/js/index.js
+++ www/js/index.js
@@ -66,11 +66,18 @@ function back() {
   history.back();
 }
 
-// 1. Capturing Photo
+// 1. Capturing / Retrieving Photo
 
 function capturePhoto() {
   // Take picture using device camera and retrieve image as base64-encoded string
   navigator.camera.getPicture(onPhotoDataSuccess, onFail, { quality: 50, correctOrientation: true });
+}
+
+function getPhoto(source) {
+  // Retrieve image file location from specified source
+  navigator.camera.getPicture(onPhotoDataSuccess, onFail, { quality: 50,
+    destinationType: destinationType.FILE_URI,
+    sourceType: source });
 }
 
 // Called when a photo is successfully retrieved
